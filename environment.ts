e177cb7f106bdfc7570b6834d7770a33cbf8f56b
oleiade <theo@crevon.me>
2025-01-31 16:43:30 +0100

    Add an environment module abstracting access to environment for k6/deno

---
+++ environment.ts
@@ -0,0 +1,22 @@
+// In the k6 runtime, the __ENV object is available and contains the environment variables.
+export declare const __ENV: Record<string, string | undefined>;
+
+/**
+ * Environment interface that matches the shape of k6's __ENV object.
+ */
+export interface Environment {
+    [key: string]: string | undefined;
+}
+
+function getEnvironment(): Environment {
+  // When running in Deno
+  if (typeof Deno !== "undefined") {
+    return Deno.env.toObject();
+  }
+
+  // When running in k6
+  return __ENV;
+}
+
+// Export a singleton instance of the environment object
+export const env: Environment = getEnvironment();
