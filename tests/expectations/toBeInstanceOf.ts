3d638b9d7d973028c1d28223eaf7211ebdce510c
Johan Suleiko Allansson <allansson@gmail.com>
2025-11-26 20:34:05 +0100

    ignore tests folder

--- tests/expectations/toBeInstanceOf.ts
+++ tests/expectations/toBeInstanceOf.ts
@@ -1,24 +1,19 @@
-import { describe, it } from "../../dist/index.js";
-import { makeExpectWithSpy } from "../helpers.ts";
+import { describe, it } from "../helpers/test.ts";
 import { dedent } from "../utils.ts";
 
 class TestClass {}
 
-it("should pass when the object is an instance of the expected class", async ({ expect }) => {
-  const [result, expectWithSpy] = makeExpectWithSpy();
+it("should pass when the object is an instance of the expected class", async ({ expect, spy }) => {
+  spy.expect(new TestClass()).toBeInstanceOf(TestClass);
 
-  expectWithSpy(new TestClass()).toBeInstanceOf(TestClass);
-
-  await expect(result.passed).toBe(true);
+  await expect(spy.result.passed).toBe(true);
 });
 
-it("should fail when the object is not an instance of the expected class", async ({ expect }) => {
-  const [result, expectWithSpy] = makeExpectWithSpy();
+it("should fail when the object is not an instance of the expected class", async ({ expect, spy }) => {
+  spy.expect({}).toBeInstanceOf(TestClass);
 
-  expectWithSpy({}).toBeInstanceOf(TestClass);
-
-  await expect(result.passed).toBe(false);
-  await expect(result.message).toEqual(dedent`
+  await expect(spy.result.passed).toBe(false);
+  await expect(spy.result.message).toEqual(dedent`
 
                    Error: expect(received).toBeInstanceOf(expected)
                       At: ...
@@ -33,21 +28,17 @@ it("should fail when the object is not an instance of the expected class", async
 });
 
 describe("not", () => {
-  it("should pass when the object is not an instance of the expected class", async ({ expect }) => {
-    const [result, expectWithSpy] = makeExpectWithSpy();
+  it("should pass when the object is not an instance of the expected class", async ({ expect, spy }) => {
+    spy.expect({}).not.toBeInstanceOf(TestClass);
 
-    expectWithSpy({}).not.toBeInstanceOf(TestClass);
-
-    await expect(result.passed).toBe(true);
+    await expect(spy.result.passed).toBe(true);
   });
 
-  it("should fail when the object is an instance of the expected class", async ({ expect }) => {
-    const [result, expectWithSpy] = makeExpectWithSpy();
+  it("should fail when the object is an instance of the expected class", async ({ expect, spy }) => {
+    spy.expect(new TestClass()).not.toBeInstanceOf(TestClass);
 
-    expectWithSpy(new TestClass()).not.toBeInstanceOf(TestClass);
-
-    await expect(result.passed).toBe(false);
-    await expect(result.message).toEqual(dedent`
+    await expect(spy.result.passed).toBe(false);
+    await expect(spy.result.message).toEqual(dedent`
 
                      Error: expect(received).toBeInstanceOf(expected)
                         At: ...
--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,72 +1,8 @@
-import { browser } from "k6/browser";
-import { colorize, expect as globalExpect, test } from "../dist/index.js";
+import { colorize, expect as globalExpect } from "../dist/index.js";
 
 export const expect = globalExpect.configure({
   soft: true,
 });
-
-/**
- * Extend the base test with helpers for browser testing.
- */
-const { describe, it } = test.extend({
-  defaultOptions: {},
-
-  mergeOptions: (baseOptions) => baseOptions,
-
-  createContext: async () => {
-    const browserContext = await browser.newContext();
-    const page = await browserContext.newPage();
-
-    return {
-      context: {
-        page,
-      },
-      async dispose() {
-        await page.close();
-        await browserContext.close();
-      },
-    };
-  },
-});
-
-export { describe, it };
-
-/**
- * Render an element into the body of the given page.
- */
-export function renderElement(page, tagName, attrs) {
-  return page.evaluate(([tagName, attrs]) => {
-    const el = document.createElement(tagName);
-
-    Object.entries(attrs).forEach(([name, value]) => {
-      el.setAttribute(name, value);
-    });
-
-    document.body.appendChild(el);
-  }, [tagName, attrs]);
-}
-
-export function makeExpectWithSpy() {
-  const result = {
-    passed: true,
-    message: null,
-  };
-
-  const expectFn = expect.configure({
-    colorize: false,
-    assertFn(condition, message) {
-      result.passed = condition;
-
-      // Remove
-      result.message = message.replace(/At: .*$/mg, "At: ...").replace(
-        /Line: \d+$/mg,
-        "Line: ...",
-      );
-    },
-  });
-
-  return [result, expectFn] as const;
-}
 
 export function failTest(testName: string, message: string) {
   class TestFailureError extends Error {
