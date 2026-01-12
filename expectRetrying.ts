de9a5908dc81c92aa1759f543f8a7e4bfa263db3
Andrew Slotin <andrew.slotin@grafana.com>
2026-01-12 15:56:36 +0100

    Pass timeout value to ToHaveValueErrorRenderer

--- expectRetrying.ts
+++ expectRetrying.ts
@@ -571,7 +571,7 @@ export function createLocatorExpectation(
         matcherName: "toHaveValue",
         expected: expectedValue,
         received: "unknown",
-        matcherSpecific: { isNegated },
+        matcherSpecific: { isNegated, timeout: options.timeout },
         customMessage: message,
       };
 
