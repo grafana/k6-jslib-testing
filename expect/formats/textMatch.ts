eb916b849d82ecb0841e4406e7b8850266dc5e79
Johan Suleiko Allansson <allansson@gmail.com>
2026-02-11 12:49:11 +0100

    add comment to text-match format

--- expect/formats/textMatch.ts
+++ expect/formats/textMatch.ts
@@ -13,6 +13,11 @@ function formatExpected(expected: string | RegExp): string {
   return expected instanceof RegExp ? expected.toString() : expected;
 }
 
+/**
+ * Similar to the expected-received error, but it assumes that a text match
+ * occurred. Unlike expected-received, this format will not wrap string values
+ * in quotes.
+ */
 registerFormatter("text-match", ({ expected, received }) => {
   return {
     Expected: green(formatExpected(expected)),
